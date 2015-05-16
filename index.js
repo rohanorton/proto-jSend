955816eee4ce88533b792140d3bd72021f24fd9d
Rohan Orton <rohan.orton@korelogic.co.uk>
2015-05-17 00:24:01 +0100

    [REFACTOR] broke out code into own functions

--- index.js
+++ index.js
@@ -2,16 +2,24 @@
 'use strict';
 
 module.exports = function (req, res, next) {
+
+  function sendResponse(code, json) {
+    return res.status(code).json(json);
+  }
+
+  function getSuccessCode() {
+    return req.method === 'POST' ? 201 : 200;
+  }
+
   res.jSend = function (data) {
-    var code = 200;
-    if (req.method === 'POST') {
-      code = 201;
-    }
-    return res.status(code).json({
+    var payload = {
       status: 'success',
       data: data || null
-    });
+    };
+
+    return sendResponse(getSuccessCode(), payload);
   };
+
   return next();
 };
 
