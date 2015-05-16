f3c5ce28d71935973f35eb0da3c4667f4fbde9eb
Rohan Orton <rohan.orton@korelogic.co.uk>
2015-05-16 09:42:08 +0100

    [RED] should call "next" function
    All express middleware needs to call the next function to progress
    along the chain.

--- test/index.js
+++ test/index.js
@@ -13,4 +13,12 @@ describe('jSend', function () {
   it('should be a function', function () {
     assert(typeof jSend === 'function');
   });
+  it('should call third parameter "next" function as callback', function () {
+    var called = false,
+      req = null,
+      res = null,
+      next = function () { called = true; };
+    jSend(req, res, next);
+    assert(called);
+  });
 });
