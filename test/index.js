d78e8995c5955fad16efcf5ef5159fb02e1d7495
Rohan Orton <rohan.orton@korelogic.co.uk>
2015-05-16 07:09:54 +0100

    Add test runner and first test... [RED] it should exist
    I'm developing this module using a Test Driven approach. I've decided
    that I will attempt to commit at each stage of the TDD cycle, using a
    naming convention for each commit according to where it is in the cycle:
    
    - [RED] for failing tests.
    - [GREEN] for passing tests.
    - [REFACTOR] for refactoring of code.
    - [PREMATURE] for new tests that pass first time ... these are
      undesirable, but sometimes unavoidable.
    
    The test runner I've chosen to use is mocha a test suite can be run by
    running `npm test`

---
+++ test/index.js
@@ -0,0 +1,11 @@
+/*jslint node:true indent:2 */
+/*globals describe, it */
+'use strict';
+
+var assert = require('assert');
+
+describe('jSend', function () {
+  it('should exist', function () {
+    assert(jSend);
+  });
+});
