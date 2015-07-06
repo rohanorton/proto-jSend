45c122427af0bf5e22137b283230c4acafe2d7a4
Abe <abe@abesmith.co.uk>
2015-07-06 22:06:36 +0100

    Red: add test that res.jSend.error sets res.statusCode properly from status property

--- spec/jSendSpec.js
+++ spec/jSendSpec.js
@@ -98,6 +98,11 @@ describe('jSend', function () {
           res.jSend.error({status: 500});
         }).toThrowError('res.jSend.error options validation: expected property message to be of type string (was undefined)');
       });
+      fit('should set res status code to specified status', function () {
+        res.jSend.error({status: 501, message: 'Not Implemented'});
+        assert.equal(res.statusCode, 501);
+      });
+      //it('should set res data status to error', function () {
     });
   });
 });
